3e478205ce6fb82551e8377a6cd3a785bd641bb7
mutiat <mutiat900@gmail.com>
2025-05-15 01:40:50 +0100

    updated

--- src/app/customer/page.tsx
+++ src/app/customer/page.tsx
@@ -1,22 +1,25 @@
 "use client";
 
+import { useState } from "react";
 import Sidebar from "@/components/sidebar";
 import Header from "@/components/header";
 import CustomerDetails from "@/components/customerdetails";
 
 export default function CustomerPage() {
+  const [activePage, setActivePage] = useState("Customer");
+
   return (
     <div className="flex min-h-screen bg-gray-100">
       {/* Sidebar */}
       <aside className="w-[20%] fixed h-full bg-white shadow z-50 h-screen overflow-y-auto">
-        <Sidebar />
+        <Sidebar setActivePage={setActivePage} />
       </aside>
 
       {/* Main Content */}
       <div className="ml-[20%] w-[80%] flex flex-col">
         {/* Header */}
         <div className="fixed top-0 left-[20%] w-[80%] z-40 bg-white shadow">
-          <Header />
+          <Header activePage={activePage} />
         </div>
 
         {/* Scrollable Content */}
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 type MenuItem = {
   icon: string;
